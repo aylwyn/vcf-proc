31eb43a4e0cd5c41d81ecb2c7929d459867e8b6d
aylwyn <aylwyn@cantab.net>
2014-01-14 13:26:07 +0000

    initial commit

---
+++ vcf-filt.py
@@ -0,0 +1,121 @@
+#!/software/bin/python
+# Aylwyn Scally 2013
+
+import sys
+import optparse
+#import os
+import re
+#import os.path
+#from numpy import median
+import logging
+from logging import error, warning, info, debug, critical
+
+loglevel = logging.WARNING
+
+# defaults
+VCF_FIXEDCOLS = 9
+#HWEMIN = 0.05
+
+p = optparse.OptionParser()
+p.add_option('-H', '--noheader', action='store_true', default = False)
+p.add_option('-d', '--mindep', dest = 'mindeparg', default = '10', help = 'min per-individual raw read depth [%default]')
+p.add_option('-D', '--maxdep', dest = 'maxdeparg', default = '100', help = 'max per-individual raw read depth [%default]')
+p.add_option('--n', '--min_passed', dest = 'minpass', type = 'int', default = -1, help = 'min number of samples passing depth filters [%default]')
+p.add_option('-q', '--Qmin', type = 'float', default = 20.0, help = 'min consensus quality [%default]')
+p.add_option('--MQmin', default = 10, help = 'min r.m.s. mapping quality of covering reads [%default]')
+p.add_option('--MQ0maxfrac', type = 'float', default = 0.25, help = 'max raction of reads with zero mapping quality [%default]')
+#p.add_option('--FQmin', type = 'float', default = 10.0, help = '')
+p.add_option('--PV4min', type = 'float', default = 0.0001, help = 'min p-value for strand bias, baseQ bias, mapQ bias and tail distance bias [%default]')
+p.add_option('--HWEmin', type = 'float', default = 0.0, help = 'min HWE chi^2 test p-value [%default]')
+#DP4: number of alternate bases > 2
+
+
+opt, args = p.parse_args()
+mindep = [int(x) for x in opt.mindeparg.split(',')]
+maxdep = [int(x) for x in opt.maxdeparg.split(',')]
+
+if args:
+	fin = open(args[0])
+else:
+	fin = sys.stdin
+fout = sys.stdout
+
+for line in fin:
+	if not opt.noheader:
+		sys.stdout.write(line)
+	if line.startswith('#CHROM'):
+		tok = line.split()
+		nsamp = len(tok) - VCF_FIXEDCOLS
+#		sampnames = tok[VCF_FIXEDCOLS:]
+		break
+
+if opt.minpass < 0:
+	opt.minpass = nsamp
+elif opt.minpass > nsamp:
+	error('%d samples, %d given for min number passing depth filter' % (nsamp, opt.minpass))
+	sys.exit(2)
+
+if len(mindep) > 1 and nsamp > len(mindep):
+	error('%d samples, %d values given for min depth' % (nsamp, len(mindep)))
+	sys.exit(2)
+elif len(mindep) == 1:
+	mindep += [mindep[0]] * (nsamp - 1)
+
+if len(maxdep) > 1 and nsamp > len(maxdep):
+	error('%d samples, %d values given for max depth' % (nsamp, len(maxdep)))
+	sys.exit(2)
+elif len(maxdep) == 1:
+	maxdep += [maxdep[0]] * (nsamp - 1)
+
+#depfield = 2
+for line in fin:
+	if line.startswith('#'):
+		sys.stdout.write(line)
+		continue
+	tok = line.split()
+
+#	if len(tok[4]) > 1: # multiallelic sites
+#		continue
+
+	if float(tok[5]) < opt.Qmin:
+		continue
+
+	formats = tok[VCF_FIXEDCOLS - 1].split(':')
+
+	depfield = formats.index('DP')
+	dp = [int(x.split(':')[depfield]) for x in tok[VCF_FIXEDCOLS:(VCF_FIXEDCOLS+nsamp)]]
+	passed = sum([dp[i] >= mindep[i] and dp[i] <= maxdep[i] for i in range(nsamp)])
+	if passed < opt.minpass:
+		continue
+
+	if int(re.search('MQ=([0-9]+)', tok[7]).group(1)) < opt.MQmin:
+		continue
+
+	DP = int(re.search('DP=([0-9]+)', tok[7]).group(1))
+	if int(re.search('MQ0=([0-9]+)', tok[7]).group(1)) > opt.MQ0maxfrac * DP:
+		continue
+
+#	if abs(float(re.search('FQ=(-?[0-9.]+)', tok[7]).group(1))) < opt.FQmin:
+#		continue
+
+	filt = False
+	pv4p = re.search('PV4=([0-9.e\-,]+)', tok[7])
+	if pv4p:
+		for pv in pv4p.group(1).split(','):
+			if eval(pv) < opt.PV4min:
+				filt = True
+	if filt:
+		continue
+
+#	DP4: number of alternate bases > 2 ONLY APPLIES TO VARIANTS
+
+	if nsamp > 1:
+		if opt.HWEmin > 0.0:
+			hwep = re.search('HWE=([0-9.e\-]+)', tok[7])
+			if hwep and eval(hwep.group(1)) < opt.HWEmin:
+				continue
+
+	sys.stdout.write(line)
+
+#	alfreq = eval(re.search('AF1=([0-9.e\-]+)', tok[7]).group(1))# eval since vcf uses sci notation
+#	alcount = int(re.search('AC1=([0-9]+)', tok[7]).group(1))
---
+++ vcf-proc.py
@@ -0,0 +1,229 @@
+#!/software/bin/python
+# Aylwyn Scally 2013
+#TODO: handle ./. output e.g. from vcf-merge
+
+import sys
+import optparse
+#import os
+import re
+#import os.path
+from numpy import median
+import logging
+from logging import error, warning, info, debug, critical
+
+loglevel = logging.WARNING
+
+# defaults
+VCF_FIXEDCOLS = 9
+#HWEMIN = 0.05
+
+#	fout.write('\t'.join([str(x) for x in args]) + '\n')
+
+def genotype(al1, al2):
+	return(''.join(sorted([al1, al2])))
+
+def hapsitetypes(n):
+# haploid allele combinations
+	for gt in ['0', '1']:
+		if n > 1:
+			for cc in hapsitetypes(n-1):
+				yield(''.join((gt, cc)))
+		else:
+			yield(gt)
+
+
+def dipsitetypes(n):
+# diploid genotype combinations
+# to get segregating sites drop first and last elements of returned vals
+	for gt in ['00', '01', '11']:
+		if n > 1:
+			for cc in dipsitetypes(n-1):
+				yield('-'.join((gt, cc)))
+		else:
+			yield(gt)
+
+def phases(gt):
+# diploid allele combinations
+	yield(gt)
+	if gt[0] != gt[1]:
+		yield(gt[::-1])
+
+def phasecombs(gts, sep = ''):
+# different phase combinations of diploid genotypes in gts 
+	if len(gts) > 1:
+		for cc in phasecombs(gts[1:], sep):
+			for ph in phases(gts[0]):
+				yield(sep.join((ph + cc)))
+	else:
+		for ph in phases(gts[0]):
+			yield(ph)
+
+#test
+tgt = ['AB', 'CD', 'EF']
+assert ','.join(phasecombs(tgt)) == 'ABCDEF,BACDEF,ABDCEF,BADCEF,ABCDFE,BACDFE,ABDCFE,BADCFE'
+
+#p = optparse.OptionParser(usage = '%prog [file] [--segsites] [--diversity] [-c cond_sample]')
+p = optparse.OptionParser()
+p.add_option('-H', '--header', action='store_true', default = False)
+p.add_option('--depths', action='store_true', default = False, help = 'output per-individual read depths')
+p.add_option('--indels', action='store_true', default = False, help = 'include indels')
+p.add_option('--segsites', action='store_true', default = False, help = 'output segregating site flag')
+p.add_option('--mediandep', action='store_true', default = False, help = 'output median read depth')
+p.add_option('--diversity', action='store_true', default = False, help = 'output diversity statistics')
+p.add_option('-c', '--condsamp', default = '', help = 'condition on het in sample CONDSAMP')
+p.add_option('--rates', action='store_true', default = False, help = 'output per-individual het and hom-non-ref rates')
+p.add_option('--ratesonly', action='store_true', default = False, help = 'as --rates; suppress per-site output')
+p.add_option('--vars', action='store_true', default = False, help = 'output variant sites only')
+p.add_option('--msmc', action='store_true', default = False, help = 'output msmc-format seg site separations')
+p.add_option('--alleles', action='store_true', default = False, help = 'output alleles')
+
+opt, args = p.parse_args()
+if opt.ratesonly:
+	opt.rates = True
+
+if args:
+	fin = open(args[0])
+else:
+	fin = sys.stdin
+fout = sys.stdout
+
+for line in fin:
+#	if not opt.noheader:
+#		sys.stdout.write(line)
+	if line.startswith('#CHROM'):
+		tok = line.split()
+		nsamp = len(tok) - VCF_FIXEDCOLS
+		sampnames = tok[VCF_FIXEDCOLS:]
+		if opt.condsamp:
+			for i, name in enumerate(sampnames):
+				if name.find(condsamp) >= 0:
+					condcol = i + VCF_FIXEDCOLS
+#				   print(i, condsamp, tok[condcol])
+					break
+		break
+
+if opt.header:
+	fout.write('\t'.join(['#SAMPLES'] + sampnames) + '\n')
+
+if opt.rates:
+	hetcount = [0 for i in range(nsamp)]
+	hnrcount = [0 for i in range(nsamp)]
+	depcount = [0 for i in range(nsamp)]
+	nsites = 0
+	firstcoord = ''
+
+lastchr = ''
+for line in fin:
+	if line.startswith('#'):
+#		sys.stdout.write(line)
+		continue
+	#TODO: handle new chr: reset sep, lastpos
+	if tok[0] != lastchr:
+		sep = 0
+		lastpos = -1
+		lastchr = tok[0]
+
+	tok = line.split()
+	
+	if opt.vars and tok[4] == '.':
+		continue
+
+#	sys.stdout.write(line)
+	if not opt.indels and (len(tok[3]) > 1 or re.search('INDEL', tok[7])):
+		continue
+
+#	if len(tok[4]) > 1: # multiallelic sites
+#		continue
+
+#	if nsamp > 1:
+#		if opt.HWEmin > 0.0:
+#			hwep = re.search('HWE=([0-9.e\-]+)', tok[7])
+
+	if opt.condsamp and not tok[condcol].startswith('0/1'): # condition on variant in cond_sample
+		continue
+
+	if opt.depths or opt.depths or opt.mediandep:
+		formats = tok[VCF_FIXEDCOLS - 1].split(':')
+		depfield = formats.index('DP')
+		dep = [x.split(':')[depfield] for x in tok[VCF_FIXEDCOLS:(VCF_FIXEDCOLS+nsamp)]]
+
+	if opt.depths:
+		fout.write('\t'.join(tok[0:2] + dep) + '\n')
+		continue
+
+	outvals = tok[0:2]
+
+	if (tok[4] == "."):
+		varvals = ['00' for x in tok[VCF_FIXEDCOLS:(VCF_FIXEDCOLS+nsamp)]]
+		segsite = False
+	else:
+		varvals = [x[0] + x[2] for x in tok[VCF_FIXEDCOLS:(VCF_FIXEDCOLS+nsamp)]]
+##		varvals = [x.split(':')[0].replace('/', '') for x in tok[VCF_FIXEDCOLS:(VCF_FIXEDCOLS+nsamp)]]
+#		if sdafilt:
+#			if (varvals[-1] == '01' and varvals.count('00') < nfsamp) or (varvals[-1] == '11' and varvals.count('01')): #exclude lines with sda in last sample
+#				continue
+#	outvals.append('-'.join(varvals[:nfsamp]))
+		segsite = '01' in varvals
+
+	if opt.alleles: #TODO
+		sitealleles = [tok[3]] + tok[4].split(',')
+		varalleles = [sitealleles[int(x[0])] + sitealleles[int(x[1])] for x in varvals]  
+		varvals = varalleles  
+#		print([sitealleles, varvals])
+
+	if opt.msmc:
+		pos = int(tok[1])
+		if pos > lastpos:
+			sep += 1
+		lastpos = pos
+#	ideally use FQ > 0 but vcf-subset doesn't currently adjust this
+#		fq = re.search('FQ=(-?[0-9.]+)', tok[7])
+#		if fq > 0: #het
+#		sys.stdout.write(line)
+		if segsite:
+			if len(varvals) > 1: #
+				allstr = ','.join(phasecombs(varvals))
+				fout.write('\t'.join(outvals + [str(sep)] + [allstr]) + '\n')
+			else:
+				fout.write('\t'.join(outvals + [str(sep)] + varvals) + '\n')
+#			outvals.append(str(sep))
+			sep = 0
+		continue
+
+	outvals.append('-'.join(varvals))
+
+	if opt.rates:
+		hetcount = [hetcount[i] + (varvals[i] == '01') for i in range(nsamp)]
+		hnrcount = [hnrcount[i] + (varvals[i] == '11') for i in range(nsamp)]
+		depcount = [depcount[i] + int(dep[i]) for i in range(nsamp)]
+		nsites += 1
+		if not firstcoord:
+			firstcoord = ':'.join(tok[0:2])
+		lastcoord = ':'.join(tok[0:2])
+
+	if not opt.ratesonly:
+		if opt.mediandep:
+			outvals.append('%d' % median(dep))
+
+		if opt.segsites:
+#			outvals.append(str(depfield - 1))
+			outvals.append(str(int(segsite)))
+
+		if opt.diversity:
+			alfreq = eval(re.search('AF1=([0-9.e\-]+)', tok[7]).group(1))# eval since vcf uses sci notation
+			alcount = int(re.search('AC1=([0-9]+)', tok[7]).group(1))
+			neidiv = 1 - alfreq**2 - (1 - alfreq)**2
+			outvals += [str(nsamp), str(alfreq), str(neidiv), str(alcount), str(alcount/float(nsamp))]
+
+		fout.write('\t'.join(outvals) + '\n')
+
+if opt.rates:
+	fout.write('\t'.join(['#REGION'] + [firstcoord + '-' + lastcoord]) + '\n')
+	fout.write('\t'.join(['#NSITES'] + [str(nsites)]) + '\n')
+	fout.write('\t'.join(['#MEANDEP'] + [str(float(x)/nsites) for x in depcount]) + '\n')
+	fout.write('\t'.join(['#HETCOUNT'] + [str(x) for x in hetcount]) + '\n')
+	fout.write('\t'.join(['#HETRATE'] + [str(float(x)/nsites) for x in hetcount]) + '\n')
+	fout.write('\t'.join(['#HNRCOUNT'] + [str(x) for x in hnrcount]) + '\n')
+	fout.write('\t'.join(['#HNRRATE'] + [str(float(x)/nsites) for x in hnrcount]) + '\n')
+	fout.write('\t'.join(['#HNR/HET'] + [str(float(hnrcount[i])/hetcount[i]) for i in range(nsamp)]) + '\n')
+#	fout.write('\t'.join(outvals) + '\n')
