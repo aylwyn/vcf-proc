645e782f1bace644106b85bf7ad48fdb571abc46
aylwyn <aylwyn@cantab.net>
2014-03-20 17:55:05 +0000

    Add --callmask; fix segsite detection

--- vcf-proc.py
+++ vcf-proc.py
@@ -12,7 +12,7 @@ import logging
 from logging import error, warning, info, debug, critical
 import gzip
 
-loglevel = logging.WARNING
+loglevel = logging.INFO
 
 # defaults
 VCF_FIXEDCOLS = 9
@@ -68,6 +68,7 @@ assert ','.join(phasecombs(tgt)) == 'ABCDEF,BACDEF,ABDCEF,BADCEF,ABCDFE,BACDFE,A
 #p = optparse.OptionParser(usage = '%prog [file] [--segsites] [--diversity] [-c cond_sample]')
 p = optparse.OptionParser()
 p.add_option('-H', '--header', action='store_true', default = False)
+p.add_option('--debug', action='store_true', default = False)
 p.add_option('--depths', action='store_true', default = False, help = 'output per-individual read depths')
 p.add_option('--indels', action='store_true', default = False, help = 'include indels')
 p.add_option('--segsites', action='store_true', default = False, help = 'output segregating site flag')
@@ -78,13 +79,18 @@ p.add_option('--rates', action='store_true', default = False, help = 'output per
 p.add_option('--ratesonly', action='store_true', default = False, help = 'as --rates; suppress per-site output')
 p.add_option('--vars', action='store_true', default = False, help = 'output variant sites only')
 p.add_option('--segsep', action='store_true', default = False, help = 'output seg site separations (e.g. for input to msmc)')
-p.add_option('--replacecalls', default='', help = 'file of replacement records (e.g. phased SNP calls). NOTE: no checking is done to ensure samples match')
+p.add_option('--replacecalls', default='', help = 'vcf.gz file of replacement records (e.g. phased SNP calls). NOTE: no checking is done to ensure samples match')
+p.add_option('--callmask', default='', help = 'bed.gz file of uncallable regions')
 p.add_option('--alleles', action='store_true', default = False, help = 'output alleles')
 p.add_option('--pseudodip', action='store_true', default = False, help = 'create pseudodiploids from consecutive pairs of input samples (assumed haploid so exclude hets)')
 
 opt, args = p.parse_args()
 if opt.ratesonly:
 	opt.rates = True
+
+if opt.debug:
+	loglevel = logging.DEBUG
+logging.basicConfig(format = '%(module)s:%(lineno)d:%(levelname)s: %(message)s', level = loglevel)
 
 if args:
 	fin = open(args[0])
@@ -93,12 +99,22 @@ else:
 fout = sys.stdout
 
 if opt.replacecalls:
+	info('Reading replacement calls from %s' % opt.replacecalls)
 	reprecord = {}
 	for line in gzip.open(opt.replacecalls):
 		if line.startswith('#'):
 			continue
 		tok = line.split()
 		reprecord[tok[0]+tok[1]] = tok[VCF_FIXEDCOLS-1:]
+
+if opt.callmask:
+	info('Reading uncallable regions from %s' % opt.callmask)
+	callmask = {}
+	for line in gzip.open(opt.callmask):
+		if line.startswith('#'):
+			continue
+		tok = line.split()
+		callmask[int(tok[1]) + 1] = int(tok[2])
 
 #	for line in fin:
 #		if line.startswith('#'):
@@ -188,19 +204,30 @@ for line in fin:
 #			if (varvals[-1] == '01' and varvals.count('00') < nfsamp) or (varvals[-1] == '11' and varvals.count('01')): #exclude lines with sda in last sample
 #				continue
 #	outvals.append('-'.join(varvals[:nfsamp]))
-		segsite = False
-		for gt in vargts:
-			if gt[0] != gt[2]:
-				segsite = True
-#		segsite = '01' in varvals
 
 	if opt.pseudodip:
-		if segsite:
+		hetsite = False
+		for gt in vargts:
+			if gt[0] != gt[2]:
+				hetsite = True
+				break
+		if hetsite:
 			continue
 		varvals = [varvals[i][0] + varvals[i+1][0] for i in range(0, len(varvals) - 1, 2)]
 		vargts = [vargts[i][0] + '|' + vargts[i+1][0] for i in range(0, len(vargts) - 1, 2)]
+#		print(vargts)
 
-	if opt.alleles: #TODO
+# determine if segregating site
+# ideally use FQ > 0 but vcf-subset doesn't currently adjust this
+#		fq = re.search('FQ=(-?[0-9.]+)', tok[7])
+#		if fq > 0: #het
+#		sys.stdout.write(line)
+	valstr = ''.join(varvals)
+#	print(valstr)
+	segsite = '0' in valstr and '1' in valstr
+#	print(segsite)
+
+	if opt.alleles:
 		sitealleles = [tok[3]] + tok[4].split(',')
 		varalleles = [sitealleles[int(x[0])] + sitealleles[int(x[1])] for x in varvals]  
 		varvals = varalleles  
@@ -209,20 +236,23 @@ for line in fin:
 
 	if opt.segsep:
 		pos = int(tok[1])
-		if pos > lastpos:
-			sep += 1 # assumes all callable sites are in vcf TODO otherwise sep = pos - lastpos
-		lastpos = pos
-#	ideally use FQ > 0 but vcf-subset doesn't currently adjust this
-#		fq = re.search('FQ=(-?[0-9.]+)', tok[7])
-#		if fq > 0: #het
-#		sys.stdout.write(line)
-		if segsite:
-			if len(vargts) > 1: #
+
+		while pos > lastpos:
+			if opt.callmask and lastpos in callmask:
+				print('pos = %d; Masking %d to %d' % (pos, lastpos, callmask[lastpos]))
+				lastpos = callmask[lastpos]
+			else:
+				sep += 1
+			lastpos += 1
+		if opt.callmask and lastpos in callmask:
+			print('pos = %d; Masking %d to %d' % (pos, lastpos, callmask[lastpos]))
+			lastpos = callmask[lastpos] + 1
+		elif lastpos == pos and segsite:
+			if len(vargts) > 1:
 				allstr = ','.join(phasecombs(vargts))
 				fout.write('\t'.join(outvals + [str(sep)] + [allstr]) + '\n')
 			else:
 				fout.write('\t'.join(outvals + [str(sep)] + varvals) + '\n')
-#			outvals.append(str(sep))
 			sep = 0
 		continue
 
